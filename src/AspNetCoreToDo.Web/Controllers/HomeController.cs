b3f40eeb491e89e7d71a47e777d7fd2858a5b51c
Andy Butland <abutland@thisiszone.com>
2016-08-18 09:25:54 +0100

    Set up redirect from home page

---
+++ src/AspNetCoreToDo.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCoreToDo.Web.Controllers
+{
+    using AspNetCoreToDo.Web.ViewModels.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index(TasksIndexViewModelQuery query)
+        {
+            return RedirectToAction("Index", "Tasks");
+        }
+   }
+}
